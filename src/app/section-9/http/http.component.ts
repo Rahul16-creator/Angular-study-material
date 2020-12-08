13bad5d94d3d8fe7e794e636304c8c9225d74831
Rahulkrishnan16 <rahul.krishnan@eunimart.com>
2020-12-08 11:28:01 +0530

    update

--- src/app/section-9/http/http.component.ts
+++ src/app/section-9/http/http.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HttpService } from '../http.component.service';
 import { map } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
+import { fromEvent, Observable, Subscription } from 'rxjs';
 
 
 @Component({
@@ -11,6 +11,14 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./http.component.css']
 })
 export class HttpComponent implements OnInit, OnDestroy {
+
+  public onlineEvent: Observable<Event>;
+  public offlineEvent: Observable<Event>;
+  public subscriptions: Subscription[] = [];
+  public connectionStatusMessage: string;
+  public connectionStatus: string;
+
+
 
   constructor(private httpService: HttpService) { }
 
@@ -22,12 +30,34 @@ export class HttpComponent implements OnInit, OnDestroy {
   error = null;
   private sub: Subscription;
   ngOnInit(): void {
+
+
+    this.onlineEvent = fromEvent(window, 'online');
+    this.offlineEvent = fromEvent(window, 'offline');
+    this.subscriptions.push(this.onlineEvent.subscribe(event => {
+      this.connectionStatusMessage = 'Connected to internet! You are online';
+      this.connectionStatus = 'online';
+    }));
+    this.subscriptions.push(this.offlineEvent.subscribe(e => {
+      this.connectionStatusMessage = 'Connection lost! You are offline';
+      this.connectionStatus = 'offline';
+    }));
+
+
+
+
+
     this.isError = false;
 
     this.sub = this.httpService.errroMessage.subscribe(res => {
       this.isError = true;
       this.error = res;
     })
+
+
+
+
+
 
   }
 
@@ -73,6 +103,10 @@ export class HttpComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.sub.unsubscribe();
+
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+
+
   }
 
 }
